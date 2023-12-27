6696f9675e68c45be10fe2fbfea203d0ff60af2a
Carlos Baez <chillboy.omg@outlook.com>
2023-12-27 10:21:56 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿int numPrimo = 2;
+bool validacion = true;
+
+Console.WriteLine("Cuantos numeros primos deseas?...");
+int n = int.Parse(Console.ReadLine());
+
+int contador = 0;
+
+do
+{
+    validacion = true;
+
+    for (int i = 2; i < numPrimo; i++)
+    {
+        if (numPrimo % i == 0)
+        {
+            //Console.WriteLine("No es un numero primo...");
+            validacion = false;
+            numPrimo++;
+            break;
+        }
+
+    }
+
+    if (validacion)
+    {
+        //Console.WriteLine("Es un numero primo...");
+        Console.WriteLine(numPrimo);
+        contador++;
+        numPrimo++;
+    }
+} while (contador < n);
